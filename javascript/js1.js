b28ec63590ebd3d7487f04d886db399cf231af64
dharmapra2 <dharmapra865@gmail.com>
2021-01-01 12:11:26 +0530

    init

---
+++ javascript/js1.js
@@ -0,0 +1,13 @@
+// $(document).ready(function(){
+//     let hamberger= document.querySelector('.hamberger');
+//     let times= document.querySelector('.times');
+//     let mobileNav= document.querySelector('.mobile-nav');
+    
+//     hamberger.addEventListerner('click', function(){
+//         mobileNav.classList.add('open');
+//     });
+//     times.addEventListerner('click', function(){
+//         mobileNav.classList.remove('open');
+//     });
+     
+//     });
